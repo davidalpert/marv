0bdf116ba51201fe2791e1f40f3393f217b6901d
David Alpert <david@spinthemoose.com>
2013-12-27 11:02:49 -0600

    initial proof-of-concept that uses a browser control to render an html document

---
+++ src/Marv/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using PropertyChanged;
+
+namespace Marv
+{
+    [ImplementPropertyChanged]
+    public class MainWindowViewModel
+    {
+        public string Html { get; set; }
+
+        public MainWindowViewModel()
+        {
+            var fileSystem = new System.IO.Abstractions.FileSystem();
+            string pathToSource = @".\sample.html";
+            var html = fileSystem.File.ReadAllText(pathToSource);
+            Html = html;
+        }
+    }
+}
---
+++ src/Marv/Xaml/BrowserBehavior.cs
@@ -0,0 +1,38 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Marv.Xaml
+{
+    /// <summary>
+    /// Adds an Html property that can be set with a string of HTML text
+    /// </summary>
+    /// <remarks>
+    /// source: http://victor.stodell.se/2012/02/binding-html-formatted-string-to-wpf.html
+    /// </remarks>
+    public class BrowserBehavior
+    {
+        public static readonly DependencyProperty HtmlProperty = DependencyProperty.RegisterAttached(
+            "Html", typeof(string), typeof(BrowserBehavior), new FrameworkPropertyMetadata(OnHtmlChanged));
+ 
+        [AttachedPropertyBrowsableForType(typeof(WebBrowser))]
+        public static string GetHtml(WebBrowser browser)
+        {
+            return (string)browser.GetValue(HtmlProperty);
+        }
+ 
+        public static void SetHtml(WebBrowser browser, string value)
+        {
+            browser.SetValue(HtmlProperty, value);
+        }
+ 
+        static void OnHtmlChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            var browser = dependencyObject as WebBrowser;
+            if (browser != null)
+            {
+                var html = e.NewValue as string ?? string.Empty;
+                browser.NavigateToString(html);
+            }
+        }
+    }
+}
